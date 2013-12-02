3643154ecf57f024f362b63292f0de3c10d98cf2
erin <festethejester@gmail.com>
2013-12-01 19:52:06 -0800

    get bins using piecewise exponential cdf

--- analyze-sorites.r
+++ analyze-sorites.r
@@ -55,56 +55,71 @@ concrete.data = aggregate(response ~ dollars + item, data=data[data$qtype == "va
 # #(sometimes it picks back up, but this method wouldn't be able to capture that)
 maxes = list(watch=3000, headphones=1000, laptop=5000, sweater=400)
 maxes[["coffee maker"]] = 400
+#nbins = 50
+maxbinwidth = 10000
+#maxresponse=8.4
 
-get.prior.bins = function(item) {
-  item.data = concrete.data[concrete.data$item == item,]
-  vals = sort(item.data$dollars)
-  #vals = c(vals, maxes[[item]])
-  vals = c(vals, seq(vals[length(vals)], maxes[[item]], length.out=10))
-  responses = sort(item.data$response)
-  #responses = c(responses, 9)
-#   plot(vals, responses, main=item)
-  responses = c(responses, seq(responses[length(responses)], 9, length.out=10)) #highest expensive rating is 9
+#################cumulants: 1-exp(alpha*x)
+
+#################cumulants: linear
+# get.prior.bins = function(item) {
+#   item.data = concrete.data[concrete.data$item == item,]
+#   vals = sort(item.data$dollars)
+#   #extra.bins = nbins-length(vals)
+#   #vals = c(vals, maxes[[item]])
+#   #vals = c(vals, vals[length(vals)]+maxbinwidth)
+#   #vals = c(vals, seq(vals[length(vals)], maxes[[item]], length.out=extra.bins)[2:extra.bins])
+#   responses = sort(item.data$response)
+#   #responses = c(responses, 9)
+#   #responses = c(responses, maxresponse)
+# #   plot(vals, responses, main=item)
+#   #responses = c(responses, seq(responses[length(responses)], 9, length.out=extra.bins)[2:extra.bins]) #highest expensive rating is 9
 #   if (item == "watch") {
 #     plot(vals, responses)
 #   }
-  get.lower = function(i, lst) {
-    if (i == 1) {
-      return(0)
-    } else {
-      return(lst[i-1])
-    }
-  }
-  bin.widths = sapply(1:length(vals), function(i) {
-    lower.x = get.lower(i, vals)
-    upper.x = vals[i]
-    return(upper.x - lower.x)
-  })
-  bin.heights = sapply(1:length(responses), function(i) {
-    lower.y = get.lower(i, responses)
-    upper.y = responses[i]
-    return(upper.y - lower.y)
-  })
-  bin.mids = sapply(1:length(vals), function(i) {
-    lower.x = get.lower(i, vals)
-    upper.x = vals[i]
-    return((upper.x - lower.x)/2 + lower.x)
-  })
-  bin.props = sapply(1:length(bin.mids), function(i) {
-    #return(bin.widths[i] * bin.heights[i])
-    return(bin.heights[i])
-  })
-  plot(bin.mids, bin.props, main=item)
-#   if (item == "watch") {
-#     plot(bin.mids, bin.props, main=item)
+#   get.lower = function(i, lst) {
+#     if (i == 1) {
+#       return(0)
+#     } else {
+#       return(lst[i-1])
+#     }
 #   }
-}
+#   bin.widths = sapply(1:length(vals), function(i) {
+#     lower.x = get.lower(i, vals)
+#     upper.x = vals[i]
+#     return(upper.x - lower.x)
+#   })
+#   bin.heights = sapply(1:length(responses), function(i) {
+#     lower.y = get.lower(i, responses)
+#     upper.y = responses[i]
+#     return(upper.y - lower.y)
+#   })
+#   bin.mids = sapply(1:length(vals), function(i) {
+#     lower.x = get.lower(i, vals)
+#     upper.x = vals[i]
+#     return((upper.x - lower.x)/2 + lower.x)
+#   })
+#   bin.props = sapply(1:length(bin.mids), function(i) {
+#     #return(bin.widths[i] * bin.heights[i])
+#     return(bin.heights[i])
+#   })
+#   #plot(bin.mids, bin.props, main=item)
+# #   if (item == "watch") {
+# #     plot(bin.mids, bin.props, main=item)
+# #   }
+#   write(item, "hi", append=T)
+#   write(bin.mids, "hi", append=T, ncol=length(bin.mids))
+#   write(bin.props, "hi", append=T, ncol=length(bin.props))
+#   return(list(mids=bin.mids, heights=bin.props))
+# }
+# 
+# items = unique(as.character(data$item))
+# 
+# prior.bins = lapply(items, get.prior.bins)
+# names(prior.bins) = items
 
-items = unique(as.character(data$item))
 
-prior.bins = lapply(items, get.prior.bins)
-
-
+#############dunno
 # 
 # get.smoothed.prior = function(item) {
 #   nbins = 50
@@ -153,139 +168,139 @@ prior.bins = lapply(items, get.prior.bins)
 # 
 # smoothed.priors = lapply(items, get.smoothed.prior)
 # 
-# # expt.pdf = lapply(c("laptop", "watch"), function(item) {
-# #     
-# #   })
-# # names(expt.pdf) = c("laptop", "watch")
+expt.pdf = lapply(c("laptop", "watch"), function(item) {
+    
+  })
+names(expt.pdf) = c("laptop", "watch")
+
+est.kernel <- function(dist, bw) {
+  k = list()
+  k$x = grid
+  k$y = predict(expt.pdf[[dist]], grid)$y/((sum(predict(expt.pdf[[dist]])$y, grid)*(grid[2]-grid[1])))
+  return(k)
+}
+
+
+
+
+
+graph.df <- function(df, label, range, save=F, file.names=c("",""), item.label="",
+                     ind.xlab="epsilons (in standard deviations)",
+                     ind.ylab="goodness of inductive premise",
+                     conc.ylab="goodness of concrete premise",
+                     conc.xlab="values (in standard deviations above the mean)") {
+  
+  avg.data <- aggregate(response ~ sigs + qtype, data=df, FUN=mean)
+  nsubj <- length(unique(data$subj))
+  conf.data <- aggregate(response ~ sigs + qtype, data=df, FUN=function(v) {
+    sample.means <- replicate(100, mean(sample(v, nsubj, replace=TRUE)))
+    return(quantile(sample.means, c(0.025, 0.975)))
+  })
+  
+  epsilons <- avg.data$sigs[avg.data$qtype=="eps"]
+  eps.y <- avg.data$response[avg.data$qtype=="eps"]
+  eps.low <- conf.data$response[avg.data$qtype=="eps",1]
+  eps.high <- conf.data$response[avg.data$qtype=="eps",2]
+  values <- avg.data$sigs[avg.data$qtype=="val"]
+  val.y <- avg.data$response[avg.data$qtype=="val"]
+  val.low <- conf.data$response[avg.data$qtype=="val",1]
+  val.high <- conf.data$response[avg.data$qtype=="val",2]
+  if (is.na(label)) {
+    inductive.label <- item.label#paste("I", item.label)
+    concrete.label <- ""#paste("C", item.label)
+  } else {
+    inductive.label <- paste(c("Sorites Expt - Inductive Premise Falloff", label), collapse="")
+    concrete.label <- paste(c("Sorites Expt - Concrete Premise Prior", label), collapse="")
+  }
+  lwd<-2
+  inductive <- plot(epsilons, eps.y, ylim=range, type="l", ylab=ind.ylab,
+                    xlab=ind.xlab, font.main=32,
+                    main=inductive.label, lwd=lwd)
+  arrows(epsilons, eps.high, epsilons, eps.low, angle=90, code=3, length=0.1, lwd=lwd)
+  concrete <- plot(values, val.y, ylim=range, type="l", xlab=conc.xlab, font.main=32,
+                   main=concrete.label, lwd=lwd, ylab=conc.ylab)
+  arrows(values, val.high, values, val.low, angle=90, code=3, length=0.1, lwd=lwd)
+}
+
+################ z scores
+z.subj <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  n.qs <- length(questions[[s]])
+  if (n.qs != 40 && n.qs != 44) {print("error2")}
+  return(rep(subj, n.qs))
+}))
+z.response <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  return(scale(data$response[data$subj==subj])[,1])
+}))
+z.qnum <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  data$qnum[data$subj==subj]
+}))
+z.qtype <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  data$qtype[data$subj==subj]
+}))
+z.dollars <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  data$dollars[data$subj==subj]
+}))
+z.sigs <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  data$sigs[data$subj==subj]
+}))
+z.item <- unlist(sapply(1:length(subjects), function(s) {
+  subj <- subjects[[s]]
+  data$item[data$subj==subj]
+}))
+z.data <- data.frame(subj=z.subj, qnum=z.qnum, qtype=z.qtype, dollars=z.dollars,
+                     sigs=z.sigs, item=z.item, response=z.response)
 # # 
-# # est.kernel <- function(dist, bw) {
-# #   k = list()
-# #   k$x = grid
-# #   k$y = predict(expt.pdf[[dist]], grid)$y/((sum(predict(expt.pdf[[dist]])$y, grid)*(grid[2]-grid[1])))
-# #   return(k)
-# # }
-# 
-# 
-# 
 # # 
-# # 
-# # graph.df <- function(df, label, range, save=F, file.names=c("",""), item.label="",
-# #                      ind.xlab="epsilons (in standard deviations)",
-# #                      ind.ylab="goodness of inductive premise",
-# #                      conc.ylab="goodness of concrete premise",
-# #                      conc.xlab="values (in standard deviations above the mean)") {
-# #   
-# #   avg.data <- aggregate(response ~ sigs + qtype, data=df, FUN=mean)
-# #   nsubj <- length(unique(data$subj))
-# #   conf.data <- aggregate(response ~ sigs + qtype, data=df, FUN=function(v) {
-# #     sample.means <- replicate(100, mean(sample(v, nsubj, replace=TRUE)))
-# #     return(quantile(sample.means, c(0.025, 0.975)))
-# #   })
-# #   
-# #   epsilons <- avg.data$sigs[avg.data$qtype=="eps"]
-# #   eps.y <- avg.data$response[avg.data$qtype=="eps"]
-# #   eps.low <- conf.data$response[avg.data$qtype=="eps",1]
-# #   eps.high <- conf.data$response[avg.data$qtype=="eps",2]
-# #   values <- avg.data$sigs[avg.data$qtype=="val"]
-# #   val.y <- avg.data$response[avg.data$qtype=="val"]
-# #   val.low <- conf.data$response[avg.data$qtype=="val",1]
-# #   val.high <- conf.data$response[avg.data$qtype=="val",2]
-# #   if (is.na(label)) {
-# #     inductive.label <- item.label#paste("I", item.label)
-# #     concrete.label <- ""#paste("C", item.label)
-# #   } else {
-# #     inductive.label <- paste(c("Sorites Expt - Inductive Premise Falloff", label), collapse="")
-# #     concrete.label <- paste(c("Sorites Expt - Concrete Premise Prior", label), collapse="")
-# #   }
-# #   lwd<-2
-# #   inductive <- plot(epsilons, eps.y, ylim=range, type="l", ylab=ind.ylab,
-# #                     xlab=ind.xlab, font.main=32,
-# #                     main=inductive.label, lwd=lwd)
-# #   arrows(epsilons, eps.high, epsilons, eps.low, angle=90, code=3, length=0.1, lwd=lwd)
-# #   concrete <- plot(values, val.y, ylim=range, type="l", xlab=conc.xlab, font.main=32,
-# #                    main=concrete.label, lwd=lwd, ylab=conc.ylab)
-# #   arrows(values, val.high, values, val.low, angle=90, code=3, length=0.1, lwd=lwd)
-# # }
-# # 
-# # ################ z scores
-# # z.subj <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   n.qs <- length(questions[[s]])
-# #   if (n.qs != 40 && n.qs != 44) {print("error2")}
-# #   return(rep(subj, n.qs))
-# # }))
-# # z.response <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   return(scale(data$response[data$subj==subj])[,1])
-# # }))
-# # z.qnum <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   data$qnum[data$subj==subj]
-# # }))
-# # z.qtype <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   data$qtype[data$subj==subj]
-# # }))
-# # z.dollars <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   data$dollars[data$subj==subj]
-# # }))
-# # z.sigs <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   data$sigs[data$subj==subj]
-# # }))
-# # z.item <- unlist(sapply(1:length(subjects), function(s) {
-# #   subj <- subjects[[s]]
-# #   data$item[data$subj==subj]
-# # }))
-# # z.data <- data.frame(subj=z.subj, qnum=z.qnum, qtype=z.qtype, dollars=z.dollars,
-# #                      sigs=z.sigs, item=z.item, response=z.response)
-# # # # 
-# # # # 
-# # # graph.df(data, "", c(1,9))
-# # # graph.df(z.data, " (z-scored)", c(-1.5,1.5))
-# # # # par(mfcol=c(2,5))
-# # # # sapply(unique(as.character(data$item)), function(lalala) {
-# # # #   graph.df(subset(z.data, z.data$item==lalala), label=NA,
-# # # #            range=c(-1.5,1.5), item.label=lalala)
-# # # # })
-# # png("sorites-items.png", 2000, 800, pointsize=28)
+# graph.df(data, "", c(1,9))
+# graph.df(z.data, " (z-scored)", c(-1.5,1.5))
 # # par(mfcol=c(2,5))
-# # sapply(unique(as.character(data$item)), function(kind.of.item) {
-# #   if (kind.of.item == "laptop") {
-# #     ind.ylab <- "goodness of inductive premise"
-# #     ind.xlab <- "epsilons (standard deviations)"
-# #     conc.xlab <- "values (standard deviations above the mean)"
-# #     conc.ylab <- "goodness of concrete premise"
-# #   } else {
-# #     ind.ylab <- ""
-# #     ind.xlab <- ""
-# #     conc.xlab <- ""
-# #     conc.ylab <- ""
-# #   }
-# #   graph.df(subset(data, data$item==kind.of.item), label=NA,
-# #            range=c(1,9), item.label=kind.of.item, ind.xlab=ind.xlab,
-# #            ind.ylab=ind.ylab, conc.xlab=conc.xlab, conc.ylab=conc.ylab)
+# # sapply(unique(as.character(data$item)), function(lalala) {
+# #   graph.df(subset(z.data, z.data$item==lalala), label=NA,
+# #            range=c(-1.5,1.5), item.label=lalala)
 # # })
-# # dev.off()
-# # 
-# # 
-# # png("sorites-z-items.png", 2000, 800, pointsize=28)
-# # par(mfcol=c(2,5))
-# # sapply(unique(as.character(z.data$item)), function(kind.of.item) {
-# #   if (kind.of.item == "laptop") {
-# #     ind.ylab <- "goodness of inductive premise"
-# #     ind.xlab <- "epsilons (standard deviations)"
-# #     conc.xlab <- "values (standard deviations above the mean)"
-# #     conc.ylab <- "goodness of concrete premise"
-# #   } else {
-# #     ind.ylab <- ""
-# #     ind.xlab <- ""
-# #     conc.xlab <- ""
-# #     conc.ylab <- ""
-# #   }
-# #   graph.df(subset(z.data, data$item==kind.of.item), label=NA,
-# #            range=c(-2,1.5), item.label=kind.of.item, ind.xlab=ind.xlab,
-# #            conc.xlab=conc.xlab, ind.ylab=ind.ylab, conc.ylab=conc.ylab)
-# # })
-# # dev.off()
+png("sorites-items.png", 2000, 800, pointsize=28)
+par(mfcol=c(2,5))
+sapply(unique(as.character(data$item)), function(kind.of.item) {
+  if (kind.of.item == "laptop") {
+    ind.ylab <- "goodness of inductive premise"
+    ind.xlab <- "epsilons (standard deviations)"
+    conc.xlab <- "values (standard deviations above the mean)"
+    conc.ylab <- "goodness of concrete premise"
+  } else {
+    ind.ylab <- ""
+    ind.xlab <- ""
+    conc.xlab <- ""
+    conc.ylab <- ""
+  }
+  graph.df(subset(data, data$item==kind.of.item), label=NA,
+           range=c(1,9), item.label=kind.of.item, ind.xlab=ind.xlab,
+           ind.ylab=ind.ylab, conc.xlab=conc.xlab, conc.ylab=conc.ylab)
+})
+dev.off()
+
+
+png("sorites-z-items.png", 2000, 800, pointsize=28)
+par(mfcol=c(2,5))
+sapply(unique(as.character(z.data$item)), function(kind.of.item) {
+  if (kind.of.item == "laptop") {
+    ind.ylab <- "goodness of inductive premise"
+    ind.xlab <- "epsilons (standard deviations)"
+    conc.xlab <- "values (standard deviations above the mean)"
+    conc.ylab <- "goodness of concrete premise"
+  } else {
+    ind.ylab <- ""
+    ind.xlab <- ""
+    conc.xlab <- ""
+    conc.ylab <- ""
+  }
+  graph.df(subset(z.data, data$item==kind.of.item), label=NA,
+           range=c(-2,1.5), item.label=kind.of.item, ind.xlab=ind.xlab,
+           conc.xlab=conc.xlab, ind.ylab=ind.ylab, conc.ylab=conc.ylab)
+})
+dev.off()
---
+++ sorites-bins.r
@@ -0,0 +1,197 @@
+library(rjson)
+
+################# reading and cleaning data for all pieces
+setwd("~/sorites-analysis/")
+rd <- read.table("sorites.results", sep="\t", quote='"', header=TRUE)
+
+subjects <- rd$workerid
+nsubj <- length(subjects)
+
+questions <- lapply(as.character(rd$Answer.questions), fromJSON)
+n.qs <- length(questions[[1]])
+
+getProp <- function(id) {
+  unlist(sapply(questions, function(question.set){
+    return(c(sapply(question.set, function(q) {
+      return(q[[id]])
+    })))
+  }))
+}
+
+subjs <- unlist(sapply(1:length(subjects), function(s){
+  subj <- subjects[[s]]
+  n.qs <- length(questions[[s]])
+  if (n.qs != 40 && n.qs != 44) {print("error1")}
+  return(rep(subj,n.qs))
+}))
+qnum <- getProp("qNumber")
+qtype <- as.character(getProp("qType"))
+dollars <- getProp("dollarAmt")
+sigs <- getProp("sigs")
+item <- getProp("item")
+response <- getProp("response")
+
+data <- data.frame(subj=subjs, #worker id
+                   qnum=qnum, #order of presentation
+                   qtype=qtype, #val for "an X that costs value is expensive"
+                   #or eps for "an X that costs epsilon less than
+                   #and expensive X is expensive"
+                   dollars=dollars, #eps or val in dollars
+                   sigs=as.numeric(sigs), #eps or val in standard deviations
+                   item=item, #headphones, coffee maker, watch, sweater, laptop
+                   response=as.numeric(response)) #1 (disagree) to 9 (agree)
+data$dollars = as.numeric(substr(as.character(data$dollars), 2, length(as.character(data$dollars))))
+concrete.data = aggregate(response ~ dollars + item, data=data[data$qtype == "val",], FUN=mean)
+
+items = c("watch", "laptop", "coffee maker", "headphones", "sweater")
+
+get.bins = function(item) {
+  #################cumulants: A + (9-A)*(1-exp(-lambda*x))
+  ##reset "lower" as the new "zero" and still approach the same asymptote (9)
+  #item = "watch"
+  item.data = concrete.data[concrete.data$item == item,]
+  vals = sort(item.data$dollars)
+  responses = sort(item.data$response)
+  get.lower = function(i, lst) {
+    if (i == 1) {
+      return(0)
+    } else {
+      return(lst[i-1])
+    }
+  }
+  seps = sapply(1:length(vals), function(i) {
+    return(vals[i] - get.lower(i, vals))
+  })
+  sep = min(seps)/4 ###can change from 2 to some larger number for more bins
+  
+  ##plot original points:
+  xlim = c(0, 2*max(vals))
+  ylim = c(0, 9)
+  plot(vals, responses, main=item, ylim=ylim, xlim=xlim)
+  
+  ##fit exponential cdf:
+  params = sapply(1:length(vals), function(i) {
+    lower.x = get.lower(i, vals)
+    upper.x = vals[i]
+    lower.y = get.lower(i, responses)
+    upper.y = responses[i]
+    a = lower.x
+    b = upper.x
+    A = lower.y
+    B = upper.y
+    lambda = (log(9-B) - log(9-A)) / (a-b)
+    
+    ##plot piecewise exponential:
+    par(new=T)
+    if (i == length(vals)) {
+      x = seq(a, xlim[2], length.out=60)
+    } else {
+      x = seq(a, b, length.out=30)
+    }
+    y = A + (9-A)*(1-exp(-lambda*(x-a)))
+    plot(x,y,ylim=ylim,xlim=xlim,xlab="",ylab="",type="l")
+    par(new=F)
+    
+    return(c(lambda, a, A)) #in equation: y = A + (9-A)*(1-exp(-lambda*(x-a)))
+  })
+  
+  #figure out which indices in params to use for a given x value
+  get.ind = function(x) {
+    for (i in 1:length(vals)) {
+      if (x < vals[i]) {
+        return(i)
+      }
+    }
+    return(length(vals)) #after the highest val, just use the last parameters
+  }
+  get.max.x = function() {
+    p = ncol(params)
+    lambda = params[1,p]
+    a = params[2,p]
+    A = params[3,p]
+    return( log(1 - (8.9-A)/(9-A)) / (-lambda) + a )
+  }
+  max.x = get.max.x()
+  bin.bounds = seq(sep, max.x, sep)
+  bin.mids = sapply(1:length(bin.bounds), function(i) {
+    lower.x = get.lower(i, bin.bounds)
+    upper.x = bin.bounds[i]
+    return((upper.x + lower.x) / 2)
+  })
+  bin.props = sapply(1:length(bin.mids), function(i) {
+    lower.x = get.lower(i, bin.bounds)
+    upper.x = bin.bounds[i]
+    mid.x = bin.mids[i]
+    p = get.ind(mid.x)
+    lambda = params[1,p]
+    a = params[2,p]
+    A = params[3,p]
+    lower.y = A + (9-A)*(1-exp(-lambda*(lower.x-a)))
+    upper.y = A + (9-A)*(1-exp(-lambda*(upper.x-a)))
+    return(upper.y - lower.y) #difference between cdf evaluated at endpoints of bin = probability on that bin
+  })
+  
+  #plot pdf:
+  #par(new=T)
+  plot(bin.mids, bin.props, ylim=c(0,max(bin.props)), main=item, type="l",
+       #col="blue",
+       ylab="", xlab="",
+       #yaxt="n", xlim=xlim
+       )
+  #par(new=F)
+  bin = list(mids=bin.mids, props=bin.props)
+}
+
+bins = lapply(items, get.bins)
+names(bins) = items
+
+print("watch")
+print(bins[["watch"]]$mids)
+print(bins[["watch"]]$props)
+print("laptop")
+print(bins[["laptop"]]$mids)
+print(bins[["laptop"]]$props)
+print("coffee maker")
+print(bins[["coffee maker"]]$mids)
+print(bins[["coffee maker"]]$props)
+print("headphones")
+print(bins[["headphones"]]$mids)
+print(bins[["headphones"]]$props)
+print("sweater")
+print(bins[["sweater"]]$mids)
+print(bins[["sweater"]]$props)
+
+
+#   bin.widths = sapply(1:length(vals), function(i) {
+#     lower.x = get.lower(i, vals)
+#     upper.x = vals[i]
+#     return(upper.x - lower.x)
+#   })
+#   bin.heights = sapply(1:length(responses), function(i) {
+#     lower.y = get.lower(i, responses)
+#     upper.y = responses[i]
+#     return(upper.y - lower.y)
+#   })
+#   bin.mids = sapply(1:length(vals), function(i) {
+#     lower.x = get.lower(i, vals)
+#     upper.x = vals[i]
+#     return((upper.x - lower.x)/2 + lower.x)
+#   })
+#   bin.props = sapply(1:length(bin.mids), function(i) {
+#     #return(bin.widths[i] * bin.heights[i])
+#     return(bin.heights[i])
+#   })
+#   #plot(bin.mids, bin.props, main=item)
+# #   if (item == "watch") {
+# #     plot(bin.mids, bin.props, main=item)
+# #   }
+#   write(item, "hi", append=T)
+#   write(bin.mids, "hi", append=T, ncol=length(bin.mids))
+#   write(bin.props, "hi", append=T, ncol=length(bin.props))
+#   return(list(mids=bin.mids, heights=bin.props))
+# }
+# 
+# items = unique(as.character(data$item))
+# 
+# prior.bins = lapply(items, get.prior.bins)
+# names(prior.bins) = items
