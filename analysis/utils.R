c56535be3f49fb3440c3bfed649d40dfc5dcc848
Erin Bennett <erindb@stanford.edu>
2018-10-03 20:04:01 -0700

    catch lognormal density error

--- analysis/utils.R
+++ analysis/utils.R
@@ -26,3 +26,12 @@ ci.low <- function(x) {
   quantile(bootstrap::bootstrap(1:length(x),1000,theta,x)$thetastar,.025)}
 ci.high <- function(x) {
   quantile(bootstrap::bootstrap(1:length(x),1000,theta,x)$thetastar,.975)}
+
+named_vec = function(df, label_vec, value_vec) {
+  if (!is.null(df)) {
+    label_vec <- df[[deparse(substitute(label_vec))]]
+    value_vec <- df[[deparse(substitute(value_vec))]]
+  }
+  names(value_vec) = label_vec
+  return(value_vec)
+}
