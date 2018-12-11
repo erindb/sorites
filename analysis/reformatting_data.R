0a2b4a5b3f20bedf2cf0df5b287ef1ed11294275
Erin Bennett <erindb@stanford.edu>
2018-12-11 10:43:25 -0800

    clean up data summary

--- analysis/reformatting_data.R
+++ analysis/reformatting_data.R
@@ -1,128 +1,122 @@
 source("utils.R")
 
-load_give_a_number = function(re_run = F) {
-  if (re_run) {
-    give_a_number = read.csv(data_dir("/priors/give-a-number.csv")) %>%
-      filter(item != "electric.kettle") %>%
-      select(-X) %>%
-      mutate(object = ifelse(item == "coffee.maker", "coffee maker", char(item))) %>%
-      select(-item)
-    write.csv(give_a_number,
-              data_dir("priors/give_a_number_reformatted.csv"),
-              row.names = F)
-  } else {
-    give_a_number = read.csv(
-      data_dir("priors/give_a_number_reformatted.csv"))
-  }
+load_give_a_number = function() {
+  give_a_number = read.csv(data_dir("/priors/give-a-number.csv")) %>%
+    filter(item != "electric.kettle") %>%
+    select(-X) %>%
+    mutate(object = ifelse(item == "coffee.maker", "coffee maker", char(item))) %>%
+    select(-item)
   return(give_a_number)
 }
 
-load_sorites = function(re_run = F) {
+load_sorites = function() {
   ## this is the script for reformatting sorites data into one single datafile
-  if (re_run) {
-    sorites_designs = read.csv(data_dir("sorites/sorites_designs.csv"),
-                               stringsAsFactors = F)
-    lookup_date = function(experiment_id) {
-      return((
-        sorites_designs %>%
-          filter(char(id) == experiment_id)
-      )$date[[1]])
-    }
-    find_sorites_filename = function(experiment_id) {
-      directory = data_dir("sorites/")
-      filenames = list.files(directory)
-      filename = filenames[
-        grep(paste("data_exp", experiment_id, sep=""),
-             filenames)
-        ]
-      return(paste(directory, filename, sep = ""))
-    }
-    read_sorites_data = function(experiment_id) {
-      raw_df_all_columns = read.csv(
-        find_sorites_filename(experiment_id),
-        stringsAsFactors = F
-      )
-
-      ## for experiment 10 data format:
-      if ("Answer.responses" %in% names(raw_df_all_columns)) {
-        raw_df_all_columns$Answer.questions = raw_df_all_columns$Answer.responses
-      }
-      if (!("Answer.phrasing" %in% names(raw_df_all_columns))) {
-        raw_df_all_columns$Answer.phrasing = NA
-      }
-      if ("Answer.time_in_minutes" %in% names(raw_df_all_columns)) {
-        raw_df_all_columns$time_in_minutes = raw_df_all_columns$Answer.time_in_minutes
-      } else {
-        raw_df_all_columns$time_in_minutes = NA
-      }
-      if ("Answer.subj_data" %in% names(raw_df_all_columns)) {
-        subj_data = do.call(rbind, lapply(raw_df_all_columns$Answer.subj_data, fromJSON)) %>%
-          as.data.frame
-        raw_df_all_columns$Answer.comments = unlist(subj_data$comments)
-        raw_df_all_columns$Answer.language = unlist(subj_data$language)
-        raw_df_all_columns$Answer.education = unlist(subj_data$education)
-        raw_df_all_columns$Answer.age = unlist(subj_data$age)
-      } else {
-        raw_df_all_columns$Answer.education = NA
-      }
-
-      ## select only the useful columns
-      raw_df = raw_df_all_columns %>%
-        rename(comments = Answer.comments,
-               language = Answer.language,
-               questions = Answer.questions,
-               education = Answer.education,
-               phrasing = Answer.phrasing,
-               age = Answer.age) %>%
-        select(workerid, reward, comments,
-               age, language, questions) %>%
-        mutate(workerid = paste(workerid, experiment_id, sep="_"))
-
-      reformatted_df = do.call(rbind, mapply(function(qn, workerid_for_merge) {
-        return(fromJSON(qn) %>% mutate(workerid = workerid_for_merge))
-      }, raw_df$questions, raw_df$workerid, SIMPLIFY = F) %>% unname)
-      ## some versions collect reaction time, etc.:
-      if (!("rt" %in% names(reformatted_df))) { reformatted_df$rt = NA }
-      if (!("level" %in% names(reformatted_df))) { reformatted_df$level = NA }
-      if (!("sigs" %in% names(reformatted_df))) { reformatted_df$sigs = NA }
-      if (!("qNumber" %in% names(reformatted_df))) { reformatted_df$qNumber = NA }
-      if ("qType" %in% names(reformatted_df)) { reformatted_df = reformatted_df%>%rename(qtype=qType) }
-      if ("dollarAmt" %in% names(reformatted_df)) {
-        reformatted_df = reformatted_df %>% rename(dollar_amount=dollarAmt)
-      }
-      if ("item" %in% names(reformatted_df)) {
-        reformatted_df = reformatted_df %>% rename(object=item)
-      }
-      df = merge(
-        raw_df %>% select(-questions),
-        reformatted_df,
-        by = c("workerid")
-      ) %>%
-        mutate(id = experiment_id,
-               date = lookup_date(experiment_id))
-      if (!("phrasing" %in% names(df))) { df$phrasing = NA }
-      return(df)
-    }
-    df = do.call(rbind, lapply(c("00", "01", "07a", "07b", "07c", "10", "11"), read_sorites_data)) %>%
-      as.data.frame %>% flatten %>% ungroup %>% mutate(
-        dollar_amount = as.numeric(unname(sapply(char(dollar_amount), function(money_string) {
-          if (substr(money_string, 1, 1) == "$") {
-            return(substr(money_string, start = 2, stop = nchar(money_string)))
-          } else {
-            return(money_string)
-          }
-        }))),
-        qtype = ifelse(qtype == "eps", "inductive", qtype),
-        qtype = ifelse(qtype == "val", "concrete", qtype),
-        qtype = factor(qtype),
-        phrasing = ifelse(id %in% c("00", "01"), "relative", phrasing),
-        response = num(response)
-      )
-
-    write.csv(df, data_dir("sorites/combined_reformatted_sorites_data.csv"), row.names = F)
-  } else {
-    df = read.csv(data_dir("sorites/combined_reformatted_sorites_data.csv"))
+  sorites_designs = read.csv(data_dir("sorites/sorites_designs.csv"),
+                             stringsAsFactors = F)
+  lookup_date = function(experiment_id) {
+    return((
+      sorites_designs %>%
+        filter(char(id) == experiment_id)
+    )$date[[1]])
   }
+  find_sorites_filename = function(experiment_id) {
+    directory = data_dir("sorites/")
+    filenames = list.files(directory)
+    filename = filenames[
+      grep(paste("data_exp", experiment_id, sep=""),
+           filenames)
+      ]
+    return(paste(directory, filename, sep = ""))
+  }
+  read_sorites_data = function(experiment_id) {
+    raw_df_all_columns = read.csv(
+      find_sorites_filename(experiment_id),
+      stringsAsFactors = F
+    )
+
+    ## for experiment 10 data format:
+    if ("Answer.responses" %in% names(raw_df_all_columns)) {
+      raw_df_all_columns$Answer.questions = raw_df_all_columns$Answer.responses
+    }
+    if (!("Answer.phrasing" %in% names(raw_df_all_columns))) {
+      raw_df_all_columns$Answer.phrasing = NA
+    }
+    if ("Answer.time_in_minutes" %in% names(raw_df_all_columns)) {
+      raw_df_all_columns$time_in_minutes = raw_df_all_columns$Answer.time_in_minutes
+    } else {
+      raw_df_all_columns$time_in_minutes = NA
+    }
+    if ("Answer.subj_data" %in% names(raw_df_all_columns)) {
+      subj_data = do.call(rbind, lapply(
+        raw_df_all_columns$Answer.subj_data,
+        function(x) {
+          x = fromJSON(x)
+          x$asses = NULL
+          return(x)
+        })) %>%
+        as.data.frame
+      raw_df_all_columns$Answer.comments = unlist(subj_data$comments)
+      raw_df_all_columns$Answer.language = unlist(subj_data$language)
+      raw_df_all_columns$Answer.education = unlist(subj_data$education)
+      raw_df_all_columns$Answer.age = unlist(subj_data$age)
+    } else {
+      raw_df_all_columns$Answer.education = NA
+    }
+
+    ## select only the useful columns
+    raw_df = raw_df_all_columns %>%
+      rename(comments = Answer.comments,
+             language = Answer.language,
+             questions = Answer.questions,
+             education = Answer.education,
+             phrasing = Answer.phrasing,
+             age = Answer.age) %>%
+      select(workerid, reward, comments,
+             age, language, questions) %>%
+      mutate(workerid = paste(workerid, experiment_id, sep="_"))
+
+    reformatted_df = do.call(rbind, mapply(function(qn, workerid_for_merge) {
+      return(fromJSON(qn) %>% mutate(workerid = workerid_for_merge))
+    }, raw_df$questions, raw_df$workerid, SIMPLIFY = F) %>% unname)
+    ## some versions collect reaction time, etc.:
+    if (!("rt" %in% names(reformatted_df))) { reformatted_df$rt = NA }
+    if (!("level" %in% names(reformatted_df))) { reformatted_df$level = NA }
+    if (!("sigs" %in% names(reformatted_df))) { reformatted_df$sigs = NA }
+    if (!("qNumber" %in% names(reformatted_df))) { reformatted_df$qNumber = NA }
+    if ("qType" %in% names(reformatted_df)) { reformatted_df = reformatted_df%>%rename(qtype=qType) }
+    if ("dollarAmt" %in% names(reformatted_df)) {
+      reformatted_df = reformatted_df %>% rename(dollar_amount=dollarAmt)
+    }
+    if ("item" %in% names(reformatted_df)) {
+      reformatted_df = reformatted_df %>% rename(object=item)
+    }
+    df = merge(
+      raw_df %>% select(-questions),
+      reformatted_df,
+      by = c("workerid")
+    ) %>%
+      mutate(id = experiment_id,
+             date = lookup_date(experiment_id))
+    if (!("phrasing" %in% names(df))) { df$phrasing = NA }
+    return(df)
+  }
+  df = do.call(rbind,
+               lapply(c("00", "01", "07a", "07b", "07c", "10", "11"),
+                      read_sorites_data)) %>%
+    as.data.frame %>% flatten %>% ungroup %>% mutate(
+      dollar_amount = as.numeric(unname(sapply(char(dollar_amount), function(money_string) {
+        if (substr(money_string, 1, 1) == "$") {
+          return(substr(money_string, start = 2, stop = nchar(money_string)))
+        } else {
+          return(money_string)
+        }
+      }))),
+      qtype = ifelse(qtype == "eps", "inductive", qtype),
+      qtype = ifelse(qtype == "val", "concrete", qtype),
+      qtype = factor(qtype),
+      phrasing = ifelse(id %in% c("00", "01"), "relative", phrasing),
+      response = num(response)
+    )
   df = df %>% mutate(
     phrasing = ifelse(phrasing == '"relative"', "relative", phrasing),
     phrasing = ifelse(phrasing == "relative_clause", "relative", phrasing),
@@ -133,84 +127,71 @@ load_sorites = function(re_run = F) {
   return(df)
 }
 
-load_priors = function(re_run = F) {
-  if (re_run) {
-    transform_upper_bounds = function(uppers) {
-      numeric_uppers = num(uppers[uppers != "infty"])
-      highest_ub = max(numeric_uppers)
-      next_highest_ub = max(numeric_uppers[numeric_uppers!=highest_ub])
-      return(ifelse(uppers=="infty",
-                    highest_ub + (highest_ub - next_highest_ub),
-                    num(uppers)))
-    }
-    process_bins_data = function(filename) {
-      raw_exp = read.csv(filename)
-      exp = raw_exp %>% filter(Answer.cond!="split") %>%
-        gather("question", "responses", c(Answer.1, Answer.2, Answer.3, Answer.4))
-      if ("Answer.35" %in% names(raw_exp)) {
-        exp_split = raw_exp %>% filter(Answer.cond=="split") %>%
-          gather("question", "responses", c(
-            Answer.1, Answer.2, Answer.3, Answer.4, Answer.5,
-            Answer.6, Answer.7, Answer.8, Answer.9, Answer.10,
-            Answer.11, Answer.12, Answer.13, Answer.14, Answer.15,
-            Answer.16, Answer.17, Answer.18, Answer.19, Answer.20,
-            Answer.21, Answer.22, Answer.23, Answer.24, Answer.25,
-            Answer.26, Answer.27, Answer.28, Answer.29, Answer.30,
-            Answer.31, Answer.32, Answer.33, Answer.34, Answer.35
-          ))
-        exp = rbind(exp, exp_split)
-      }
-      list_data = mapply(
-        function(response, question, workerid, a5, a6, a7, a8, a9, condition) {
-          # char(a5) %>% fromJSON # we also collected a max price
-          rs = response %>% fromJSON %>% as.data.frame %>%
-            mutate(workerid = workerid,
-                   condition = condition[[1]])
-          rs = rs %>% mutate(bin = 1:nrow(rs))
-          if ("lower" %in% names(rs)) {
-            rs = rs %>% rename(lowers = lower)
-          }
-          if ("upper" %in% names(rs)) {
-            rs = rs %>% rename(uppers = upper)
-          }
-          if ("response" %in% names(rs)) {
-            rs = rs %>% rename(responses = response)
-          }
-          return(rs)
-        },
-        exp$responses, exp$question, exp$workerid,
-        exp$Answer.5, exp$Answer.6, exp$Answer.7, exp$Answer.8, exp$Answer.9,
-        exp$Answer.cond,
-        SIMPLIFY=F)
-      df = do.call(rbind, list_data) %>%
-        group_by(item) %>%
-        mutate(
-          UB = transform_upper_bounds(uppers),
-          LB = num(lowers),
-          width = UB-LB,
-          rating = num(responses)/sum(num(responses))) %>%
-        ungroup() %>%
-        select(workerid, item, bin, UB, LB, rating, condition) %>%
-        as.data.frame
-      row.names(df) = 1:nrow(df)
-      return(df)
-    }
-    df3 = process_bins_data(data_dir("/priors/data_exp03_2013_12_03_05.csv")) %>% mutate(exp="03")
-    df4 = process_bins_data(data_dir("/priors/data_exp03_2013_12_03_05.csv")) %>% mutate(exp="04")
-    df5 = process_bins_data(data_dir("/priors/data_exp05_2013_12_04_15.csv")) %>% mutate(exp="05")
-    df6 = process_bins_data(data_dir("/priors/data_exp06_2013_12_04.csv")) %>% mutate(exp="06")
-    df8 = process_bins_data(data_dir("/priors/data_exp08_2014_01_31_11.csv")) %>% mutate(exp="08")
-    prior_bins_data = do.call(rbind, list(df3, df4, df5, df6, df8)) %>% as.data.frame %>%
-      mutate(exp = factor(exp),
-             condition = factor(ifelse(condition=="\"split\"", "split", "original"))) %>%
-      group_by(workerid, item, exp, condition) %>%
-      mutate(
-        slider_total = sum(rating),
-        normed_rating = ifelse(slider_total==0, 0, rating / slider_total)) %>%
-      ungroup %>% as.data.frame
-    write.csv(prior_bins_data, cache_dir("prior_bins_data_cache.csv"), row.names = F)
-  } else {
-    prior_bins_data = read.csv(cache_dir("prior_bins_data_cache.csv"))
+load_priors = function() {
+  transform_upper_bounds = function(uppers) {
+    numeric_uppers = num(uppers[uppers != "infty"])
+    highest_ub = max(numeric_uppers)
+    next_highest_ub = max(numeric_uppers[numeric_uppers!=highest_ub])
+    return(ifelse(uppers=="infty",
+                  highest_ub + (highest_ub - next_highest_ub),
+                  uppers) %>% num)
   }
+  process_bins_data = function(filename) {
+    raw_exp = read.csv(filename, stringsAsFactors = F)
+    exp = raw_exp %>% filter(Answer.cond!="split") %>%
+      gather("question", "responses", c(Answer.1, Answer.2, Answer.3, Answer.4))
+    if ("Answer.35" %in% names(raw_exp)) {
+      exp_split = raw_exp %>% filter(Answer.cond=="split") %>%
+        gather("question", "responses", contains("Answer"))
+      exp = rbind(exp, exp_split)
+    }
+    list_data = mapply(
+      function(response, question, workerid, a5, a6, a7, a8, a9, condition) {
+        # char(a5) %>% fromJSON # we also collected a max price
+        rs = response %>% fromJSON %>% as.data.frame %>%
+          mutate(workerid = workerid,
+                 condition = condition[[1]])
+        rs = rs %>% mutate(bin = 1:nrow(rs))
+        if ("lower" %in% names(rs)) {
+          rs = rs %>% rename(lowers = lower)
+        }
+        if ("upper" %in% names(rs)) {
+          rs = rs %>% rename(uppers = upper)
+        }
+        if ("response" %in% names(rs)) {
+          rs = rs %>% rename(responses = response)
+        }
+        return(rs)
+      },
+      exp$responses, exp$question, exp$workerid,
+      exp$Answer.5, exp$Answer.6, exp$Answer.7, exp$Answer.8, exp$Answer.9,
+      exp$Answer.cond,
+      SIMPLIFY=F)
+    df = do.call(rbind, list_data) %>%
+      group_by(item) %>%
+      mutate(
+        UB = transform_upper_bounds(uppers),
+        LB = num(lowers),
+        width = UB-LB,
+        rating = num(responses)/sum(num(responses))) %>%
+      ungroup() %>%
+      select(workerid, item, bin, UB, LB, rating, condition) %>%
+      as.data.frame
+    row.names(df) = 1:nrow(df)
+    return(df)
+  }
+  df3 = process_bins_data(data_dir("/priors/data_exp03_2013_12_03_05.csv")) %>% mutate(exp="03")
+  df4 = process_bins_data(data_dir("/priors/data_exp03_2013_12_03_05.csv")) %>% mutate(exp="04")
+  df5 = process_bins_data(data_dir("/priors/data_exp05_2013_12_04_15.csv")) %>% mutate(exp="05")
+  df6 = process_bins_data(data_dir("/priors/data_exp06_2013_12_04.csv")) %>% mutate(exp="06")
+  df8 = process_bins_data(data_dir("/priors/data_exp08_2014_01_31_11.csv")) %>% mutate(exp="08")
+  prior_bins_data = do.call(rbind, list(df3, df4, df5, df6, df8)) %>% as.data.frame %>%
+    mutate(exp = factor(exp),
+           condition = factor(ifelse(condition=="\"split\"", "split", "original"))) %>%
+    group_by(workerid, item, exp, condition) %>%
+    mutate(
+      slider_total = sum(rating),
+      normed_rating = ifelse(slider_total==0, 0, rating / slider_total)) %>%
+    ungroup %>% as.data.frame
   return (prior_bins_data);
 }
